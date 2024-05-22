7fc3b74bd1b65bbfc35e9d4e81fe748881bb2a4f
Netnet78 <sysophaneth78@gmail.com>
2024-05-22 19:39:59 +0700

    updated-the-dynamic-functions

--- main.js
+++ main.js
@@ -1,21 +1,5 @@
-class students {
-    constructor(name, age, gender, email, grade) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-        this.email = email;
-        this.grade = grade;
-    };
-    getStudentData() {
-        console.log("Student name: " + this.name);
-        console.log("Student age: " + this.age);
-        console.log("Student gender: " + this.gender);
-        console.log("Student Email: " + this.email);
-        console.log("Student's grade: " + this.grade + '\n');
-        console.log("========================== \n");
-    };
+const logoImage = document.getElementById("logo-img").innerHTML;
+function redirectToProject() {
+    window.open("netnet78.github.io");
 };
-
-var student1 = new students("Sy Sophaneth", 16, "Male", "sysophaneth@gmail.com", 'B');
-
-student1.getStudentData();
+logoImage.addEventListener("click", redirectToProject);
