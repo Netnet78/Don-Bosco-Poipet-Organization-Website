086b90a00ae676a39a62e1f458ace0e42b6751c9
Netnet78 <sysophaneth78@gmail.com>
2024-05-26 15:54:11 +0700

    addedComments

--- main.js
+++ main.js
@@ -1,24 +1,7 @@
 const logoImage = document.getElementById("logo-img");
-function redirectToProject() {
+// Shortcut for storing image without having to write DOM element
+function redirectToProject() { // When called this function will redirect the user in the following link:
     window.open("https://netnet78.github.io/");
 };
 logoImage.addEventListener("click", redirectToProject);
-
-const userData = () => {
-    let username = document.getElementById("username").textContent;
-    let password = document.getElementById("password").textContent;
-    let confirmPass = document.getElementById("confirmPassword").textContent;
-    if (username.length > 2) {
-        if (password.length > 7) {
-            if (password === confirmPass) {
-                console.log("Sign up successful!");
-            } else {
-                console.log("Sign up failed!");
-            }
-        } else {
-            console.log("Password must be 8 charaters long or more");
-        }
-    } else {
-        console.log("Username must be 3 characters long or more")
-    }
-}
+// Adding functionality to redirect user to another website when click
