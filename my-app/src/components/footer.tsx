7bee829be3af01461c4731889a350ce1aa64d033
Netnet78 <sysophaneth78@gmail.com>
2024-10-11 17:17:55 +0700

    Added footer to the website

--- my-app/src/components/footer.tsx
+++ my-app/src/components/footer.tsx
@@ -1,6 +1,9 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMapPin } from "@fortawesome/free-solid-svg-icons";
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faLink } from "@fortawesome/free-solid-svg-icons";
+import { faFacebook, faYoutube } from "@fortawesome/free-brands-svg-icons";
+import "../app/globals.css";
 
 /* Footer Elements
     Contact Information
@@ -26,45 +29,49 @@ import { faPhone } from "@fortawesome/free-solid-svg-icons";
 
     Copyright Notice
         A statement indicating copyright ownership (e.g., "© 2024 [Organization Name]. All rights reserved.")
-
-    Privacy Policy & Terms of Service
-        Links to your website’s privacy policy and terms of service
-
-    Site Map
-        A brief site map for easy navigation
-
-    Accreditations or Certifications
-        Logos or badges of any relevant accreditations or partnerships that lend credibility to your organization
-
-    Funding Partners or Sponsors
-        Logos of sponsors or partners who support your mission (if applicable)
-
-    Call to Action
-        A brief statement encouraging users to take action (e.g., "Join us in making a difference!" with a link to the donation page)
 */
+
+const currentYear = new Date().getUTCFullYear();
+
 const PageFooter = () => {
     return (
-        <footer className="sm:h-48 bg-slate-800 text-white flex flex-col items-center justify-between p-4 text-sm">
-            <div className="flex items-center w-auto my-0 justify-center mb-0">
+        <footer className="sm:h-80 bg-slate-800 text-white flex flex-col items-center justify-between p-4 text-sm">
+            <div className="flex items-center w-auto my-0 justify-center mb-0 cursor-default">
                 <FontAwesomeIcon icon={faMapPin} className="h-5 w-5 mr-2"/>
                 <b className="ml-1 mr-1">Mittapheap</b>Village,
                 <b className="ml-1 mr-1">Poipet</b>Commune, 
                 <b className="ml-1 mr-1">Poipet</b> City,
                 <b className="ml-1 mr-1">Banteay Meanchey</b>Province
             </div>
-            <div className="flex items-center w-auto my-0 justify-center">
+            <div className="flex items-center w-auto my-0 justify-center cursor-default">
                 <FontAwesomeIcon icon={faPhone} className="h-5 w-5 mr-2"/>
                 <span className="ml-1">Phone Number: +855 78 581 695</span>
             </div>
-            <hr className="border-t-4 border-white my-0 w-full"/>
+            <hr className="border-t-4 border-white my-0 w-4/5 text-center"/>
+            <span className="flex items-center w-auto my-0 justify-center cursor-default">
+                <h4 className="text-sm font-bold text-center">Quick Links</h4>
+                <FontAwesomeIcon icon={faLink} className="h-5 w-5 ml-2"/>
+            </span>
             <div className="flex items-center w-auto my-0 justify-center">
-                <h4 className="text-sm font-bold text-center mb-5">Quick Links</h4>
                 <div className="flex flex-wrap justify-center space-x-3">
-                    <a href="/">Home</a>
-                    <a href="/about">About Us</a>
-                    <a href="/programs">Programs</a>
-                    <a href="/admission">Admission</a>
+                    <a href="/" className="underline-animation text-white">Home</a>
+                    <a href="/about" className="underline-animation text-white">About Us</a>
+                    <a href="/programs" className="underline-animation text-white">Programs</a>
+                    <a href="/admission" className="underline-animation text-white">Admission</a>
                 </div>
+            </div>
+            <hr className="border-t border-gray-500 w-1/2 text-center"/>
+            <div className="flex items-center w-auto my-0 justify-center">
+                <h4>Follow our social media!</h4>
+                <a href="https://www.youtube.com/channel/UCu8lJrOjfnKtummPuJoGEcg/featured" target="_blank">
+                    <FontAwesomeIcon icon={faYoutube} className="h-5 w-5 mx-2 rotation-animation cursor-pointer"/>
+                </a>
+                <a href="https://web.facebook.com/donboscocenterpoipet" target="_blank">
+                    <FontAwesomeIcon icon={faFacebook} className="h-5 w-5 mx-2 rotation-animation cursor-pointer"/>
+                </a>
+            </div>
+            <div className="flex items-center w-auto mb-1 mt-3 justify-center p-4 border-2">
+                <h4 className="cursor-default">© {currentYear} Don Bosco Poipet. All rights reserved.</h4>
             </div>
         </footer>
     );
